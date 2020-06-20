2a6dca35f01aed1c07984a925d8843922f4bccf6
bayek0fsiwa <imvikas7526@gmail.com>
2020-06-20 21:44:39 +0530

    Initial Commit

---
+++ twitter.py
@@ -0,0 +1,31 @@
+#imprt Modules
+import tweepy
+import time
+
+#Authentication Keys
+auth = tweepy.OAuthHandler('Api Key', 'APi secret key')
+auth.set_access_token('acces token', 'access token secret')
+
+#Don't Get Banned Add Time Limit For Making Request Through API
+api = tweepy.api(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+#Authenticate
+user = api.me()
+
+
+#Serach For Tweet To Like or Retweet
+search = 'Python'
+numberTweets = 500
+
+#Loop Through All The Tweets in the (numberTweets) range
+for tweet in tweepy.Cursor(api.search, search).items(numberTweets):
+    try:
+        print('Tweet Liked')
+        tweet.favorite()
+        time.sleep(3)
+        tweet.Retweet()
+        time.sleep(10)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
